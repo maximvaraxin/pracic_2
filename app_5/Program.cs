e9e99c7c87eb1051d88d0715b49d4e1960f9a8ba
Максим Вараксин <maks-02@yandex.ru>
2022-06-27 23:02:08 +0300

    add app_5

---
+++ app_5/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+ * 5, 25 -> да
+ * -4, 16 -> да 
+ * 25, 5 -> да 
+ * 8,9 -> нет
+ */
+
+namespace App_4
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите число: ");
+            double num_1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Введите число: ");
+            double num_2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Является ли одно число квадратом другого: {Sqtr(num_1, num_2)} ");
+
+        }
+
+        static string Sqtr(double number_1, double number_2)
+        {
+            string sqtr = ( (number_1 * number_1 == number_2) || (number_2 * number_2 == number_1) ) ? $"ДА" : $"НЕТ";
+
+            return sqtr;
+        }
+    }
+}
