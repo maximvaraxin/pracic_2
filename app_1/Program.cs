653b610383de3e25c4fc17c2fa81dae0caf592c1
Максим Вараксин <maks-02@yandex.ru>
2022-06-27 19:54:14 +0300

    add App_1.TestMethod

--- app_1/Program.cs
+++ app_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace App_1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            TestMethod($"Все работает");
+        }
+
+        static void TestMethod(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
