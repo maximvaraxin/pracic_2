135da1ef60e31f3e03c638fbc6fc700694aa19eb
Максим Вараксин <maks-02@yandex.ru>
2022-06-27 21:49:34 +0300

    add app_2

---
+++ app_2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+ * 456 -> 46
+ * 782 -> 72
+ * 918 -> 98
+ */
+
+namespace App_2
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            //TestMethod($"Все работает");
+            
+            DelNumber(Rand());
+   
+        }
+
+        static void TestMethod(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        static int Rand()
+        {
+           return new Random().Next(100,1000);
+        }
+
+        static void DelNumber(int num)
+        {   
+            int numDel = (num % 100) / 10;
+            
+            int num1 = (num / 100) % 10;
+            int num2 = num % 10;
+
+            Console.Write($"{num1}{num2}");
+        }
+    }
+}
